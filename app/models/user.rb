da97a1d9ebdc76655c0e9aae8e2441838e5eb41b
Lornakaboro <lornakaboro@gmail.com>
2023-06-10 18:09:47 +0300

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    has_many :likes, foreign_key: :author_id, dependent: :destroy
-    has_many :comments, foreign_key: :author_id, dependent: :destroy
-    has_many :posts, foreign_key: :author_id, dependent: :destroy
-  
-    scope :user_recent_posts, ->(user_id) { find(user_id).posts.order(created_at: :desc).limit(3) }
-  end
+  has_many :likes, foreign_key: :author_id, dependent: :destroy
+  has_many :comments, foreign_key: :author_id, dependent: :destroy
+  has_many :posts, foreign_key: :author_id, dependent: :destroy
+
+  scope :user_recent_posts, ->(user_id) { find(user_id).posts.order(created_at: :desc).limit(3) }
+end
