106503faf4c19d105748e61fbffb118b8781ea87
Lornakaboro <lornakaboro@gmail.com>
2023-06-17 22:59:46 +0300

    add styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,12 +5,12 @@ class PostsController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:user_id])
-    @post = if params[:user_id].present?
-              @user.posts.find(params[:author_id])
-            else
-              Post.find(params[:id])
-            end
+    if params[:user_id].present?
+      @user = User.find(params[:user_id])
+      @post = @user.posts.find(params[:id])
+    else
+      @post = Post.find(params[:id])
+    end
     @comments = @post.comments
   end
 end
