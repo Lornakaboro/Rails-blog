caf2f7c9f2340df6426efc1a0c02e70df3457752
Lornakaboro <lornakaboro@gmail.com>
2023-06-23 15:00:47 +0300

    Modify tests to cover scenarios for multiple users

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -4,6 +4,8 @@ RSpec.feature 'User Index Page', type: :feature do
   before do
     @user1 = User.create(name: 'Sam', photo: 'https://icons.iconarchive.com/icons/iconsmind/outline/256/User-icon.png',
                          bio: 'Teacher from Poland.', posts_counter: 1)
+    @user2 = User.create(name: 'John', photo: 'https://example.com/john.jpg',
+                         bio: 'Developer from USA.', posts_counter: 3)
     visit users_path
   end
 
@@ -11,6 +13,10 @@ RSpec.feature 'User Index Page', type: :feature do
     expect(page).to have_content(@user1.name)
     expect(page.html).to include(@user1.photo)
     expect(page).to have_content(@user1.posts_counter)
+
+    expect(page).to have_content(@user2.name)
+    expect(page.html).to include(@user2.photo)
+    expect(page).to have_content(@user2.posts_counter)
   end
 
   scenario 'When I click on a user, I am redirected to their show page' do
